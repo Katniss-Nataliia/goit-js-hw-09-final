47a0bcdc4fb2b4d13e731596eed17c93b072d3d5
Katniss-Nataliia <84478133+Katniss-Nataliia@users.noreply.github.com>
2023-06-17 19:59:06 +0000

    Deploying to gh-pages from @ Katniss-Nataliia/goit-js-hw-09-final@f94fdd14f4beabf862c16f3f74ac1b2c92dccfd6 🚀

--- index.10d9dfa3.js
+++ index.10d9dfa3.js
@@ -1,2 +1,2 @@
 !function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=null;e.disabled=!0,t.addEventListener("click",(function(){a=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0)),t.disabled=!0,e.disabled=!1}),1e3)})),e.addEventListener("click",(function(){clearInterval(a),t.disabled=!1,e.disabled=!0}))}();
-//# sourceMappingURL=01-color-switcher.24e3c1e0.js.map
+//# sourceMappingURL=index.10d9dfa3.js.map
